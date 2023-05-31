90e53586019f382ab54353118e6023949ea6c82a
Lucas Klemke <lucas.klemke@Outlook.com.br>
2023-05-31 18:10:30 -0300

    imagem div estilizada pagina produto

---
+++ js/produto.js
@@ -0,0 +1,7 @@
+// SCRIPT PRODUCTPAGE
+
+const imagem4 = document.getElementById("imagemSecundaria4");
+const imagem3 = document.getElementById("imagemSecundaria3");
+const imagem2 = document.getElementById("imagemSecundaria2");
+const imagemPrincipal = document.getElementById("imagemPrincipal");
+
