8adedef96323a639c3a88121307b06b4a7c776ec
Lucas Klemke <lucas.klemke@Outlook.com.br>
2023-06-02 10:30:15 -0300

    carrossel produto funcionando

--- js/produto.js
+++ js/produto.js
@@ -1,7 +1,79 @@
-// SCRIPT PRODUCTPAGE
 
-const imagem4 = document.getElementById("imagemSecundaria4");
-const imagem3 = document.getElementById("imagemSecundaria3");
-const imagem2 = document.getElementById("imagemSecundaria2");
-const imagemPrincipal = document.getElementById("imagemPrincipal");
 
+$(document).ready(function(){
+
+    mostrarImagemSecundaria1();
+
+    $('#imagemSecundaria1').click(function(){
+        mostrarImagemSecundaria1();
+    });
+    $('#imagemSecundaria2').click(function(){
+        mostrarImagemSecundaria2();
+    });
+    $('#imagemSecundaria3').click(function(){
+        mostrarImagemSecundaria3();
+    })
+    $('#imagemSecundaria4').click(function(){
+        mostrarImagemSecundaria4();
+    })
+});
+
+function mostrarImagemSecundaria1(){
+
+    let html = ' ';
+
+    html +='<div class="imagemPrincipal imagemDestaque1">';
+    html +='</div>';
+
+    $('#imagemPrincipal').html(html);
+
+    $('#imagemSecundaria4').removeClass('brilho')
+    $('#imagemSecundaria2').removeClass('brilho')
+    $('#imagemSecundaria3').removeClass('brilho')
+    $('#imagemSecundaria1').addClass('brilho')
+
+}
+
+function mostrarImagemSecundaria2(){
+    let html = ' ';
+
+    html +='<div class="imagemPrincipal imagemDestaque2">';
+    html +='</div>';
+
+    $('#imagemPrincipal').html(html);
+
+    $('#imagemSecundaria1').removeClass('brilho')
+    $('#imagemSecundaria3').removeClass('brilho')
+    $('#imagemSecundaria4').removeClass('brilho')
+    $('#imagemSecundaria2').addClass('brilho')
+
+}
+
+function mostrarImagemSecundaria3(){
+    let html = ' ';
+
+    html +='<div class="imagemPrincipal imagemDestaque3">';
+    html +='</div>';
+
+    $('#imagemPrincipal').html(html);
+
+    $('#imagemSecundaria1').removeClass('brilho')
+    $('#imagemSecundaria2').removeClass('brilho')
+    $('#imagemSecundaria4').removeClass('brilho')
+    $('#imagemSecundaria3').addClass('brilho')
+}
+
+function mostrarImagemSecundaria4(){
+    let html = ' ';
+
+    html +='<div class="imagemPrincipal imagemDestaque4">';
+    html +='</div>';
+
+    $('#imagemPrincipal').html(html);
+
+    $('#imagemSecundaria1').removeClass('brilho')
+    $('#imagemSecundaria2').removeClass('brilho')
+    $('#imagemSecundaria3').removeClass('brilho')
+    $('#imagemSecundaria4').addClass('brilho')
+
+}
